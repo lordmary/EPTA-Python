dcf782d6390fccdf8a965932a7d00b139d820bf4
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2022-04-20 14:35:58 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,57 @@
+inteiro = 3
+flutuante = 4.5
+char = 'a'
+
+primeiro_valor = 8
+segundo_valor = 2
+
+# multiplicacao = primeiro_valor * segundo_valor
+# divisao = flutuante // segundo_valor
+# exponenciacao = primeiro_valor ** segundo_valor
+# modulo = primeiro_valor % segundo_valor
+
+# nome_completo = meu_nome + " "+ sobrenome
+# print(multiplicacao)
+# print(divisao)
+# print(exponenciacao)
+# print(modulo)
+
+# verdadeiro = True
+# falso = False
+
+# if(falso):
+#   print("Eh verdadeiro!")
+# else:
+#   print("Nao eh verdadeiro")
+#   print("Nunca vai ser printado :(")
+
+# IMC
+# < 18,5 = MAGREZA
+# >= 18,5 E <= 24,9 NORMAL
+# >= 25 E <= 29,9 SOBREPESO
+# >29,9 OBESIDADE
+
+# altura = float(input("Digite sua altura "))
+# peso = float(input("Digite seu peso "))
+
+# imc = peso / (altura ** 2)
+
+# if(imc < 18.5):
+#   print("magreza")
+# elif (imc >= 18.5 and imc <= 25):
+#   print("normal")
+# elif (imc > 25 and imc <= 29.9):
+#   print("sobrepeso")
+# else:
+#   print("obesidade")
+
+lista_de_numeros = [1,2,3,4,5]
+lista_de_numeros.remove(4)
+
+lista_de_compras = ["banana","maca","leite","biscoito"]
+lista_de_compras.append("cebola")
+lista_de_compras.sort()
+# print(lista_de_compras)
+
+for compra in lista_de_compras:
+  print(compra)
